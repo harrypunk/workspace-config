23212a24b181f85b9092f65df1f8f1e36b88b12e
hp <allthatjazz@outlook.com>
2022-01-13 11:53:12 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,7 +1,20 @@
 set -x EDITOR vim
 
+set -x ANDROID_HOME ~/Android/Sdk
+set -x JAVA_HOME /usr/lib/jvm/default
+
+set -x PUB_HOSTED_URL https://pub.flutter-io.cn
+set -x FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
+set -x FLUTTER_GIT_URL https://gitee.com/mirrors/Flutter.git
+
+set -x PATH ~/.flutter/flutter/bin $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
+
+set -x RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
+set -x RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
+
 alias l="ls -Ahl"
 alias wk="cd ~/Workspace"
+alias ...="cd ../.."
 
 function showp
   for p in $PATH
@@ -9,4 +22,10 @@ function showp
   end
 end
 
+function sethp
+    set -gx https_proxy "http://127.0.0.1:1081"
+    set -gx http_proxy "http://127.0.0.1:1081"
+end
+
 source ~/.asdf/asdf.fish
+
